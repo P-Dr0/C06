cf34391b0cced436994d468e8f3e19c3f9b5491e
Pedro Henrique dos Santos Araujo <phsa2004@gmail.com>
2026-02-23 15:42:28 -0300

    v1

---
+++ ex01/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float x = 10;
+        float y = 4;
+        float z = 2;
+
+        float total = 0;
+        float media = 0;
+
+        total = x + y + z;
+        media = (x + y + z) / 3;
+        System.out.println("Total de lanches: " + total);
+        System.out.println("Media de lanches: " + media);
+
+    }
+}
